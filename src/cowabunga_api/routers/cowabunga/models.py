19dcb64af5d19941dd9bf8932a19414be022116d
a <a@ovid.local>
2026-04-19 08:34:03 -0500

    fix: add missing auth to cowabunga tokens and models endpoints

--- src/cowabunga_api/routers/cowabunga/models.py
+++ src/cowabunga_api/routers/cowabunga/models.py
@@ -1,10 +1,13 @@
 from fastapi import APIRouter
 from cowabunga_api.utils import get_model_config
+from cowabunga_api.routers.supabase_session import Session
 
 router = APIRouter(prefix="/cowabunga/v1/models", tags=["cowabunga/models"])
 
 
 @router.get("")
-async def models():
+async def models(
+    session: Session,  # pylint: disable=unused-argument # required for authorizing endpoint
+):
     """List all the models."""
     return get_model_config()
