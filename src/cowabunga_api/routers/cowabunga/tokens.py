7b3edbaa39ad807a6ecaf40f65469a4690ce68f7
a <a@ovid.local>
2026-04-19 08:34:03 -0500

    fix: add missing auth to cowabunga tokens and models endpoints

--- src/cowabunga_api/routers/cowabunga/tokens.py
+++ src/cowabunga_api/routers/cowabunga/tokens.py
@@ -3,6 +3,7 @@ from cowabunga_api.utils import get_model_config
 from cowabunga_api.backend.grpc_client import (
     count_tokens,
 )
+from cowabunga_api.routers.supabase_session import Session
 
 import logging
 
@@ -13,7 +14,10 @@ router = APIRouter(prefix="/cowabunga/v1/tokens", tags=["cowabunga/tokens"])
 
 
 @router.post("/count")
-async def count_tokens_route(text: str) -> int:
+async def count_tokens_route(
+    text: str,
+    session: Session,  # pylint: disable=unused-argument # required for authorizing endpoint
+) -> int:
     config = get_model_config()
     logger.info(f"Model config: {config}")
     model = config.models["text-embeddings"]
